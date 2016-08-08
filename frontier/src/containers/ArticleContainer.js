38f1ebba0ce2a8e924faa5bc603c59d9e66e0523
Christian Duvholt <cxhristian@gmail.com>
2016-08-09 00:07:57 +0200

    Add a simple edit mode toggle

--- frontier/src/containers/ArticleContainer.js
+++ frontier/src/containers/ArticleContainer.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { resizeArticle } from '../actionCreators';
 import DragArticle from './DragArticle';
+import Article from '../components/Article';
 import Resizable from 'react-resizable-box';
 
 class ArticleContainer extends React.Component {
   constructor(props) {
     super(props);
 
+    this.editMode = true;
     this.grid = [16, 16];
     this.resizableDirections = { right: true, bottom: true, bottomRight: true };
     this.onResizeStop = this.onResizeStop.bind(this);
@@ -18,7 +20,7 @@ class ArticleContainer extends React.Component {
     dispatch(resizeArticle(id, width, height));
   }
 
-  render() {
+  editComponent() {
     const { width, height } = this.props;
     return (
       <Resizable onResizeStop={ this.onResizeStop }
@@ -26,7 +28,20 @@ class ArticleContainer extends React.Component {
         grid={ this.grid } width={ width } height={ height }>
         <DragArticle { ...this.props } />
       </Resizable>
-    )
+    );
+  }
+
+  viewComponent() {
+    const { width, height } = this.props;
+    return (
+      <div style={ { width, height} }>
+        <Article { ...this.props } />
+      </div>
+    );
+  }
+
+  render() {
+    return this.editMode ? this.editComponent() : this.viewComponent();
   }
 }
 
