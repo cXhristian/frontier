ef78d4dabfe891b98626111d2b03db93c0788b2a
Sigurd Grøneng <groneng93@gmail.com>
2016-09-18 21:01:38 +0200

    Add container for new components

---
+++ src/containers/SidebaContainer.js
@@ -0,0 +1,1 @@
+import Siderbar from '../components/Siderbar';
---
+++ src/containers/SidebarContainer.js
@@ -0,0 +1,13 @@
+import Sidebar from '../components/Sidebar';
+import React from 'react';
+
+class SidebarContainer extends React.Component {
+
+  render(){
+    return(
+      <Sidebar />
+    )
+  }
+}
+
+module.exports = SidebarContainer;
