ffc577c0f5e2a819a4ccc58b578417822deee083
Sigurd Grøneng <groneng93@gmail.com>
2016-09-18 21:00:53 +0200

    Add article-type to test data

--- src/fixtures/articles.js
+++ src/fixtures/articles.js
@@ -1,3 +1,5 @@
+import { ARTICLE, VIDEO, LIVE } from '../constants/ArticleTypes';
+
 export default {
   "articles": {
     1: {
@@ -5,63 +7,72 @@ export default {
       "title": "Big article",
       "width": 544,
       "height": 448,
-      "fontSize": 20
+      "fontSize": 20,
+      "type": ARTICLE
     },
     2: {
       "id": 2,
       "title": "Small top",
       "width": 416,
       "height": 224,
-      "fontSize": 20
+      "fontSize": 20,
+      "type": ARTICLE
     },
     3: {
       "id": 3,
       "title": "Small bottom",
       "width": 416,
       "height": 224,
-      "fontSize": 20
+      "fontSize": 20,
+      "type": ARTICLE
     },
     4: {
       "id": 4,
       "title": "Test article 1",
       "width": 336,
       "height": 224,
-      "fontSize": 20
+      "fontSize": 20,
+      "type": VIDEO
     },
     5: {
       "id": 5,
       "title": "Test article 2",
       "width": 352,
       "height": 224,
-      "fontSize": 20
+      "fontSize": 20,
+      "type": LIVE
     },
     6: {
       "id": 6,
       "title": "Test article 3",
       "width": 272,
       "height": 224,
-      "fontSize": 20
+      "fontSize": 20,
+      "type": ARTICLE
     },
     7: {
       "id": 7,
       "title": "Big article",
       "width": 544,
       "height": 448,
-      "fontSize": 20
+      "fontSize": 20,
+      "type": ARTICLE
     },
     8: {
       "id": 8,
       "title": "Small top",
       "width": 416,
       "height": 224,
-      "fontSize": 20
+      "fontSize": 20,
+      "type": ARTICLE
     },
     9: {
       "id": 9,
       "title": "Small bottom",
       "width": 416,
       "height": 224,
-      "fontSize": 20
+      "fontSize": 20,
+      "type": ARTICLE
     }
   },
   "groupOrder": [1, 2, 3],
