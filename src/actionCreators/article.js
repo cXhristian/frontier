e144f1bcdb6e71446fd7cdf620525865d2bb9431
Christian Duvholt <cxhristian@gmail.com>
2016-10-19 00:19:57 +0200

    Simplify actionCreator syntax

--- src/actionCreators/article.js
+++ src/actionCreators/article.js
@@ -1,53 +1,31 @@
 import { EDIT_TITLE, MOVE_ARTICLE, RESIZE_ARTICLE, ADD_ARTICLE_TO_GROUP, CROP_IMAGE, DELETE_ARTICLE_FROM_GROUP } from '../actions';
 
-export const resizeArticle = (id, width, height) => {
-  return {
-    type: RESIZE_ARTICLE,
-    payload: {
-      id, width, height
-    }
-  }
-}
+export const resizeArticle = (id, width, height) => ({
+  type: RESIZE_ARTICLE,
+  payload: { id, width, height }
+});
 
-export const editTitle = (id, title, fontSize) => {
-  return {
-    type: EDIT_TITLE,
-    payload: {
-      id, title, fontSize
-    }
-  };
-};
+export const editTitle = (id, title, fontSize) => ({
+  type: EDIT_TITLE,
+  payload: { id, title, fontSize }
+});
 
-export const moveArticle = (id, fromGroup, toGroup, index) => {
-  return {
-    type: MOVE_ARTICLE,
-    payload: {
-      id, fromGroup, toGroup, index
-    }
-  }
-}
+export const moveArticle = (id, fromGroup, toGroup, index) => ({
+  type: MOVE_ARTICLE,
+  payload: { id, fromGroup, toGroup, index }
+});
 
-export const addArticleToGroup = (articleId, toGroup, fromGroup = null) => {
-  return {
-    type: ADD_ARTICLE_TO_GROUP,
-    payload: {
-      articleId, toGroup, fromGroup
-    }
-  }
-};
+export const addArticleToGroup = (articleId, toGroup, fromGroup = null) => ({
+  type: ADD_ARTICLE_TO_GROUP,
+  payload: { articleId, toGroup, fromGroup }
+});
 
-export const deleteArticleFromGroup = (articleId, groupId) => {
-  return {
-    type: DELETE_ARTICLE_FROM_GROUP,
-    payload: {
-      articleId, groupId
-    }
-  }
-}
+export const deleteArticleFromGroup = (articleId, groupId) => ({
+  type: DELETE_ARTICLE_FROM_GROUP,
+  payload: { articleId, groupId }
+});
 
-export const cropArticleImage = (id, cropData) => {
-  return {
-    type: CROP_IMAGE,
-    payload: { id, cropData }
-  };
-};
+export const cropArticleImage = (id, cropData) => ({
+  type: CROP_IMAGE,
+  payload: { id, cropData }
+});
--- src/actionCreators/articleGroup.js
+++ src/actionCreators/articleGroup.js
@@ -1,33 +1,19 @@
 import { MOVE_ARTICLE_GROUP, DELETE_ARTICLE_GROUP, NEW_ARTICLE_GROUP, TOGGLE_ALIGN } from '../actions';
 
-export const moveArticleGroup = (id, toIndex) => {
-  return {
-    type: MOVE_ARTICLE_GROUP,
-    payload: {
-      id, toIndex
-    }
-  }
-}
+export const moveArticleGroup = (id, toIndex) => ({
+  type: MOVE_ARTICLE_GROUP,
+  payload: { id, toIndex }
+});
 
-export const deleteArticleGroup = (id) => {
-    return {
-        type: DELETE_ARTICLE_GROUP,
-        payload: {
-            id
-        }
-    }
-}
+export const deleteArticleGroup = (id) => ({
+  type: DELETE_ARTICLE_GROUP,
+  payload: { id }
+});
 
-export const newArticleGroup = (id, index, align) => {
-    return {
-        type: NEW_ARTICLE_GROUP,
-        payload: {
-            id,
-            index,
-            align
-        }
-    }
-}
+export const newArticleGroup = (id, index, align) => ({
+  type: NEW_ARTICLE_GROUP,
+  payload: { id, index, align }
+});
 
 export const toggleAlign = (groupId) => ({
   type: TOGGLE_ALIGN,
