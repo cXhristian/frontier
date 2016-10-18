3e9151125aa784252217fec5ed538bd56cf78a17
Christian Duvholt <cxhristian@gmail.com>
2016-10-18 23:30:22 +0200

    Rewrote dispatch logic in HandlebarContainer

--- src/containers/HandlebarContainer.js
+++ src/containers/HandlebarContainer.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import Handlebar from '../components/Handlebar';
-import { deleteArticleGroup, newArticleGroup } from '../actionCreators';
+import * as Actions from '../actionCreators';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 const DEFAULT_ALIGNMENT = 'left';
 
-const mapDispatchToProps = (dispatch, props) => {
-    return {
-        deleteArticleGroup: () => {
-            dispatch(deleteArticleGroup(props.groupId))
-        },
-        newArticleGroup: () => {
-            dispatch(newArticleGroup(9, props.index, DEFAULT_ALIGNMENT)) // newId, index of this articleGroup, align
-        }
-    }
-}
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(Actions, dispatch)
+});
 
 const mapStateToProps = (state, props) => {
     return {
@@ -24,14 +18,31 @@ const mapStateToProps = (state, props) => {
 }
 
 class HandlebarContainer extends Component {
-    render(){
-        return(
-            <Handlebar
-            deleteArticleGroup={this.props.deleteArticleGroup}
-            newArticleGroup={this.props.newArticleGroup}
-             />
-        )
-    }
+  constructor(props) {
+    super(props);
+
+    this.newArticleGroup = this.newArticleGroup.bind(this);
+    this.deleteArticleGroup = this.deleteArticleGroup.bind(this);
+  }
+
+  deleteArticleGroup() {
+    const { actions, groupId } = this.props;
+    actions.deleteArticleGroup(groupId);
+  }
+
+  newArticleGroup() {
+    const { actions, index } = this.props;
+    actions.newArticleGroup(9, index, DEFAULT_ALIGNMENT); // newId, index of this articleGroup, align
+  }
+
+  render(){
+    return (
+      <Handlebar
+        deleteArticleGroup={ this.deleteArticleGroup }
+        newArticleGroup={ this.newArticleGroup }
+      />
+    )
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(HandlebarContainer)
